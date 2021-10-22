607941ea285878460eaae0e267d38c258a16c7e0
xyqweb <wsxyq001@gmail.com>
2021-10-22 17:51:42 +0800

    add upload remote oss

--- src/Oss.php
+++ src/Oss.php
@@ -15,6 +15,15 @@ use xyqWeb\oss\drivers\OssFactory;
 class Oss
 {
     /**
+     * @var \xyqWeb\oss\drivers\QiNiu|\xyqWeb\oss\drivers\AliYun
+     */
+    private static $client;
+    /**
+     * @var string
+     */
+    private static $md5String = '';
+
+    /**
      * 初始化上传底层
      *
      * @author xyq
@@ -28,10 +37,20 @@ class Oss
         if (!in_array($type, ['aliYun', 'qiNiu'])) {
             throw new \Exception('不存在oss名称');
         }
+        $md5String = $type;
+        foreach ($params as $key => $value) {
+            $md5String .= $key . '=' . $value;
+        }
+        $md5String = md5($md5String);
+        if ($md5String === self::$md5String && self::$client instanceof OssFactory) {
+            return self::$client;
+        }
+        self::$md5String = $md5String;
         $className = "xyqWeb\oss\drivers\\" . ucfirst($type);
         if (!class_exists($className)) {
             throw new \Exception('不存在oss对象');
         }
-        return new $className($params);
+        self::$client = new $className($params);
+        return self::$client;
     }
 }
